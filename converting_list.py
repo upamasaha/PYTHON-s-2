fc9b9378eb2b8e57152bb68875d4e9b98ca44573
Upama Saha <118078722+upamasaha@users.noreply.github.com>
2022-11-26 00:06:36 +0530

    Add files via upload

---
+++ converting_list.py
@@ -0,0 +1,6 @@
+## Program to make a tuple out of a list:
+
+month=["Jan","Feb","Mar","Apr","May","Jun","jul","Aug","Sep","Oct","Nov","Dec"]
+converting_list=tuple(month)
+print(converting_list)
+print(type(converting_list))
